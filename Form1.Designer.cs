9207e29bee82467864cbb08c868d55a54cf5eb7a
heathertaylor5 <heather_taylor55@hotmail.com>
2022-11-27 12:45:25 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace TaylorLab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtMultiplier = new System.Windows.Forms.TextBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblTopic = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnData = new System.Windows.Forms.Button();
+            this.btnPicture = new System.Windows.Forms.Button();
+            this.picMoose = new System.Windows.Forms.PictureBox();
+            this.lblData = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMoose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblAnswer);
+            this.groupBox1.Controls.Add(this.txtMultiplier);
+            this.groupBox1.Controls.Add(this.txtNum);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.btnCalc);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 294);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Multiplier";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAnswer.Location = new System.Drawing.Point(95, 186);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(100, 23);
+            this.lblAnswer.TabIndex = 2;
+            // 
+            // txtMultiplier
+            // 
+            this.txtMultiplier.Location = new System.Drawing.Point(95, 116);
+            this.txtMultiplier.Name = "txtMultiplier";
+            this.txtMultiplier.Size = new System.Drawing.Size(100, 27);
+            this.txtMultiplier.TabIndex = 1;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(95, 43);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 27);
+            this.txtNum.TabIndex = 0;
+            // 
+            // btnReset
+            // 
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReset.Location = new System.Drawing.Point(126, 242);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(88, 36);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(15, 242);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(88, 36);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "&Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Answer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Multiplier:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblTopic);
+            this.groupBox2.Controls.Add(this.btnExit);
+            this.groupBox2.Controls.Add(this.btnHide);
+            this.groupBox2.Controls.Add(this.btnData);
+            this.groupBox2.Controls.Add(this.btnPicture);
+            this.groupBox2.Controls.Add(this.picMoose);
+            this.groupBox2.Controls.Add(this.lblData);
+            this.groupBox2.Location = new System.Drawing.Point(269, 25);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(438, 294);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Information";
+            // 
+            // lblTopic
+            // 
+            this.lblTopic.AutoSize = true;
+            this.lblTopic.Location = new System.Drawing.Point(183, 19);
+            this.lblTopic.Name = "lblTopic";
+            this.lblTopic.Size = new System.Drawing.Size(57, 19);
+            this.lblTopic.TabIndex = 5;
+            this.lblTopic.Text = "Moose";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(330, 242);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(88, 36);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(229, 242);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(88, 36);
+            this.btnHide.TabIndex = 2;
+            this.btnHide.Text = "&Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnData
+            // 
+            this.btnData.Location = new System.Drawing.Point(128, 242);
+            this.btnData.Name = "btnData";
+            this.btnData.Size = new System.Drawing.Size(88, 36);
+            this.btnData.TabIndex = 1;
+            this.btnData.Text = "&Data";
+            this.btnData.UseVisualStyleBackColor = true;
+            this.btnData.Click += new System.EventHandler(this.btnData_Click);
+            // 
+            // btnPicture
+            // 
+            this.btnPicture.Location = new System.Drawing.Point(27, 242);
+            this.btnPicture.Name = "btnPicture";
+            this.btnPicture.Size = new System.Drawing.Size(88, 36);
+            this.btnPicture.TabIndex = 0;
+            this.btnPicture.Text = "&Picture";
+            this.btnPicture.UseVisualStyleBackColor = true;
+            this.btnPicture.Click += new System.EventHandler(this.btnPicture_Click);
+            // 
+            // picMoose
+            // 
+            this.picMoose.Image = ((System.Drawing.Image)(resources.GetObject("picMoose.Image")));
+            this.picMoose.Location = new System.Drawing.Point(27, 43);
+            this.picMoose.Name = "picMoose";
+            this.picMoose.Size = new System.Drawing.Size(393, 179);
+            this.picMoose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picMoose.TabIndex = 0;
+            this.picMoose.TabStop = false;
+            this.picMoose.Visible = false;
+            this.picMoose.Click += new System.EventHandler(this.picMoose_Click);
+            // 
+            // lblData
+            // 
+            this.lblData.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblData.Location = new System.Drawing.Point(27, 43);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(393, 179);
+            this.lblData.TabIndex = 0;
+            this.lblData.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnPicture;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnReset;
+            this.ClientSize = new System.Drawing.Size(719, 331);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab 2 by Heather Taylor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMoose)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtMultiplier;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnData;
+        private System.Windows.Forms.Button btnPicture;
+        private System.Windows.Forms.PictureBox picMoose;
+        private System.Windows.Forms.Label lblTopic;
+        private System.Windows.Forms.Label lblData;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaylorLab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* Heather Taylor - October 6th, 2022
+             * This program is both a calculator and a display of some information about my cat Moose
+             * The left side of the program will multiply 2 numbers and display the solution
+             * The right side of the program will display a photo and a short description of Moose
+             * */
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+        }
+
+        private void btnPicture_Click(object sender, EventArgs e)
+        {
+            //Show the picture and hide data label
+            picMoose.Visible = true;
+            lblData.Visible = false;
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            //hide the picture and data label on top of it
+            picMoose.Visible = false;
+            lblData.Visible = false;
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //blank out the text boxes and the answer label
+            txtNum.Text = "";
+            txtMultiplier.Text = "";
+            lblAnswer.Text = "";
+
+            //set focus to Number text box 
+            txtNum.Focus();
+        }
+
+        private void btnData_Click(object sender, EventArgs e)
+        {
+            //hide picture
+            picMoose.Visible = false;
+
+            //display information in lblData
+            lblData.Visible = true;
+
+            //description of my topic using the label
+            lblData.Text = "My topic is " + lblTopic.Text
+                + "\n"
+                + "\nMoose is a stray cat who I adopted unexpectedly in 2011."
+                + "\n"
+                + "\nShe charged at me when I called her and that's how she got her name."
+                + "\n"
+                + "\nShe isn't the friendliest cat but she cuddles up when serenaded.";
+
+        }
+
+        private void picMoose_Click(object sender, EventArgs e)
+        {
+            //display a message box when the image is clicked
+            MessageBox.Show("Meow!", "Heather Taylor");
+            
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            //declare data types for text boxes
+            int number = Convert.ToInt32(txtNum.Text);
+            int multiplier = Convert.ToInt32(txtMultiplier.Text);
+            int answer = number * multiplier;
+
+            //display calculation answer in answer label
+            lblAnswer.Text = answer.ToString();
+        }
+    }
+}
